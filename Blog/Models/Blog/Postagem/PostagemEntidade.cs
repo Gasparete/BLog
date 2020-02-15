413613cbfade4feb501ce73f3e6c006b3a9f1608
Gasparete <60801883+Gasparete@users.noreply.github.com>
2020-02-14 23:29:04 -0200

    Blog Final

---
+++ Blog/Models/Blog/Postagem/PostagemEntidade.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Models.Blog.Autor;
+using Blog.Models.Blog.Categoria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Blog.Postagem
+{
+    public class PostagemEntidade
+    {
+        public String Titulo;
+        public AutorEntidade autor;
+        public CategoriaEntidade Categoria;
+
+    }
+}
